9d53eed84ab0416cc9b72a7e38219d74ffa2d429
cffhr99 <83987504+cffhr99@users.noreply.github.com>
2021-07-21 18:57:12 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const profileDataArgs = process.argv.slice(2);
+
+const [Name,github] = profileDataArgs;
+
+// const Name = profileDataArgs[0];
+
+// const github = profileDataArgs[1];
 
 
 
 
+fs.writeFile('./index.html', generatePage(Name, github), err => {
+  if (err) throw err;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
-  };
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-
-printProfileData(profileDataArgs);
 
--- test.js
+++ test.js
@@ -26,3 +26,19 @@ const data1 = function(){
 const data2 = () => {
 
 }
+
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+
+
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
+
+
+printProfileData(profileDataArgs);
