6a33e2bf5db1b4269d2a8243c75204a1266f83f6
cffhr99 <83987504+cffhr99@users.noreply.github.com>
2021-07-21 18:12:58 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'Lernantino',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+console.log(personObj)
+
+// function types:
+function data(){
+
+}
+
+
+const data1 = function(){
+
+}
+
+
+const data2 = () => {
+
+}
